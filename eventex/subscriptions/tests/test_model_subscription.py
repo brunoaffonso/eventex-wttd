bba399b3c58a62712364cf2b063fb3686144abf4
brunoaffonso <brunoaffonso@gmail.com>
2022-02-06 10:54:25 -0300

    admin magic

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -20,4 +20,7 @@ class SubscriptionModelTest(TestCase):
         """Subscription must have an auto created at attr"""
         self.assertIsInstance(self.obj.created_at, datetime)
 
+    def test_str(self):
+        self.assertEqual('Bruno Affonso', str(self.obj))
+
 
